c715f672eca47b44a0e83937c1697173a5d1a60a
Adrian E <addis.aden@gmail.com>
2014-05-12 23:00:14 +0200

    fill with nums get his own function

--- main.c
+++ main.c
@@ -2,34 +2,40 @@
 #include <time.h>
 #include <stdlib.h>
 
-int main()
+void fill_with_numbers(int *nums, int modulo, int grenze)
 {
   // Init randomizer
   srand(time(NULL));
 
-  int numbers[4] = {-1, -1, -1, -1};
   int i, j;
 
-  for(i = 0; i < 4; i++)
+  for(i = 0; i < grenze; i++)
   {
-    if(numbers[i] >= 0) continue;
+    if(*(nums + i) >= 0) continue;
 
     while(1)
     {
-      int tmp = rand() % 10;
+      int tmp = rand() % modulo;
       int ok = 1;
 
       for(j = 0; j < i; j++)
       {
-        if(numbers[j] == tmp) ok = 0;
+        if(*(nums + j) == tmp) ok = 0;
       }
 
       if(ok) {
-        numbers[i] = tmp;
+        *(nums + i) = tmp;
         break;
       }
     }
   }
+}
+
+int main()
+{
+  int numbers[4] = {-1, -1, -1, -1};
+
+  fill_with_numbers(numbers, 10, 4);
 
   printf("%i %i %i %i\n", numbers[0], numbers[1], numbers[2], numbers[3]);
 
