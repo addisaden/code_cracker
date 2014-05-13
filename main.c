b2ee6821a0cd0125d6cb4cf756f297a4c074c108
Adrian E <addis.aden@gmail.com>
2014-05-13 23:21:05 +0200

    code is working

--- main.c
+++ main.c
@@ -1,13 +1,90 @@
 #include <stdio.h>
 #include "randomizer.h"
 
+int guess_numbers(int *nums, int range, int length)
+{
+  int guessed[length];
+  char correct[length];
+  int i, j;
+
+  printf("%i Zahlen (0 - %i):\n", length, range - 1);
+
+  // guessing
+  for(i = 0; i < length; i++)
+  {
+    scanf("%i", guessed + i);
+  }
+
+  // check the guessing
+  for(i = 0; i < length; i++)
+  {
+    correct[i] = '\0';
+
+    if(guessed[i] >= range || guessed[i] < 0)
+    {
+      correct[i] = '?';
+      continue;
+    }
+
+    else if(guessed[i] == nums[i])
+    {
+      correct[i] = 'X';
+      continue;
+    }
+
+    for(j = 0; j < length; j++)
+    {
+      if(guessed[i] == nums[j])
+      {
+        correct[i] = '+';
+        break;
+      }
+    }
+
+    if(correct[i] == '\0')
+      correct[i] = ' ';
+  }
+
+  // check for winning
+  for(i = 0; i < length; i++)
+  {
+    if(correct[i] != 'X') break;
+    if(i == length - 1) return 1;
+  }
+
+  // print corrector
+  for(i = 0; i < length; i++)
+    printf("%c ", correct[i]);
+
+  printf("   X :correct   + : move this   ? : not in range\n");
+
+  return 0;
+}
+
+
+
 int main()
 {
   int numbers[4] = {-1, -1, -1, -1};
 
   fill_with_numbers(numbers, 10, 4);
 
-  printf("%i %i %i %i\n", numbers[0], numbers[1], numbers[2], numbers[3]);
+  int solved = 0;
+  int counter = 0;
+
+  while(!(solved = guess_numbers(numbers, 10, 4)))
+  {
+    if(counter >= 10)
+      break;
+    counter++;
+  }
+
+  if(solved)
+    printf("You cracked the code in %i steps. Congratulation.\n", counter+1);
+  else
+    puts("You lost. Maybe you crack it the next time.");
+
+  printf("CODE :: %i %i %i %i\n", numbers[0], numbers[1], numbers[2], numbers[3]);
 
   return 0;
 }
